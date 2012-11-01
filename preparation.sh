e06ffb3ef18cb900827f1155ab4ef68cc1f0d4e6
Nilesh <lvnilesh@yahoo.com>
2012-10-31 19:35:46 -0700

    Aperture update

--- preparation.sh
+++ preparation.sh
@@ -8,6 +8,8 @@ hdiutil mount -quiet /Volumes/store/install/alfred_1.3.1_261.dmg && cp -R /Volum
 hdiutil mount -quiet /Volumes/store/install/Aperture/Aperture3.0Trial.dmg && sudo installer -pkg /Volumes/Aperture\ Trial\ 3.0/ApertureTrial.mpkg -target /
 #Aperture Update
 hdiutil mount -quiet /Volumes/store/install/Aperture/Aperture3.4Update.dmg && sudo installer -pkg /Volumes/Aperture\ 3.4/Aperture3.4Update.pkg -target /
+#Aperture Update
+hdiutil mount -quiet /Volumes/store/install/Aperture/Aperture3.4.1Update.dmg && sudo installer -pkg /Volumes/Aperture\ 3.4.1/Aperture3.4.1Update.pkg -target /
 
 #Dropbox
 hdiutil mount -quiet /Volumes/store/install/Dropbox\ 1.4.12.dmg && cp -R /Volumes/Dropbox\ Installer/Dropbox.app  /Applications/
