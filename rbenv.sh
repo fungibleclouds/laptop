d413ee9e5f79814a015861942a89bf3535e26815
Nilesh <nilesh@londhe.com>
2013-02-10 12:11:20 -0800

    updates

--- rbenv.sh
+++ rbenv.sh
@@ -1,5 +1,6 @@
 cd ~
-git clone git://github.com/sstephenson/rbenv.git .rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshenv
-echo 'eval "$(rbenv init -)"' >> ~/.zshenv
-exec $SHELL
+git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+exec $SHELL -l
+source ~/.zshrc
--- ruby-build.sh
+++ ruby-build.sh
@@ -1,7 +1,8 @@
 rbenv rehash
 mkdir -p ~/.rbenv/plugins
 cd ~/.rbenv/plugins
-git clone git://github.com/sstephenson/ruby-build.git
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+rbenv rehash
 rbenv install 1.9.3-p385 
 rbenv rehash
 rbenv global 1.9.3-p385 
